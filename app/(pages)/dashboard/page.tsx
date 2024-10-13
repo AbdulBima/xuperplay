f7d08cd2f54c17e10158917f247bc29c61b3261b
Bima <abdulbimadev@gmail.com>
2024-10-13 10:51:55 +0100

    14_commit

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
 const Dashboard = () => {
+
+    const projectName = localStorage.getItem("buid");
+
   return (
-    <div className=''>Dashboard</div>
+    <div className='w-full h-full flex items-center justify-center text-4xl font-semibold'>Welcom: <span className="text-orange-700">{projectName}</span></div>
   )
 }
 
